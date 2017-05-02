e3297bde9cf2f8a82d37ab66157da948c7246d91
Grady Shelton and Kat Gifford <grady@>
2017-05-02 14:16:51 -0700

    add animal sinatra routing to add animals and view them as a list

--- app.rb
+++ app.rb
@@ -29,5 +29,30 @@ post('/add/family') do
   breed_pref = params.fetch('breed_pref')
   new_family = Family.new({:name => name, :phone => phone, :species_pref => species_pref, :breed_pref => breed_pref, :id => nil})
   new_family.save
-  erb(:index)
+  @families = Family.all
+  erb(:family_list)
+end
+
+#animals
+get('/animal_list') do
+  @animals = Animal.all
+  erb(:animal_list)
+end
+
+get('/add/animal') do
+  erb(:add_animal)
+end
+
+post('/add/animal') do
+  name = params.fetch('name')
+  date_in = params.fetch('date_in')
+  species = params.fetch('species')
+  breed = params.fetch('breed')
+  gender = params.fetch('gender')
+  age = params.fetch('age')
+  family_id = 0
+  new_animal = Animal.new({:name => name, :date_in => date_in, :species => species, :breed => breed, :gender => gender, :age => age, :family_id => family_id})
+  new_animal.save
+  @animals = Animal.all
+  erb(:animal_list)
 end
